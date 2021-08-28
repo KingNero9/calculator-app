1e4799cdf5a14b592148468f6aab88f68e70d95c
Tali Shushan <talishush@gmail.com>
2021-08-28 16:49:14 -0700

    added js functions

--- calc.js
+++ calc.js
@@ -1,3 +1,18 @@
+
+function clearScreen() {
+    document.getElementById("result").value = "";
+}
+
+function display(value) {
+    document.getElementById("result").value += value;
+}
+
+function calculate() {
+    var p = document.getElementById("result").value;
+    var q = eval(p); //evaluates the string in js and executes it
+    document.getElementById("result").value = q;
+}
+
 function myFunction() {
     var element = document.body;
     element.classList.toggle("dark-mode");
